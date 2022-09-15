2b209fc4eeda437dfc8fc900a8d0472589daddba
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-15 16:56:06 +0430

    Command Pattern was developed.

---
+++ 02-CommandPattern/CommandPattern/Data/DataReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Data;
+
+public class DataReceiver
+{
+	private readonly Dictionary<string,string> _data;
+	
+	public DataReceiver()
+	{
+		_data = new Dictionary<string,string>();
+	}
+
+	public void Upsert(string key, string value)
+	{
+		_data[key] = value;
+		Console.WriteLine($"Upserted: {key} - {value}");
+	}
+
+	public void Delete(string key)
+	{
+		if (_data.ContainsKey(key))
+		{
+			_data.Remove(key);
+			Console.WriteLine($"Removed: {key}");
+		}
+	}
+}
---
+++ 02-CommandPattern/CommandPattern/Infrastructures/Command.cs
@@ -0,0 +1,23 @@
+﻿using CommandPattern.Data;
+using CommandPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Infrastructures;
+
+public class Command : ICommand
+{
+    protected DataReceiver _dataReceiver;
+
+    public Command(DataReceiver dataReceiver)
+    {
+        _dataReceiver = dataReceiver;
+    }
+    public virtual void Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
